7e609e0bad28c71d1d8482dbdfe12d3dcce20b4e
srijon <86311499+srijonp4@users.noreply.github.com>
2025-03-08 02:18:20 +0530

    initial commit

--- cmd/02-Values/main.go
+++ cmd/02-Values/main.go
@@ -1,3 +1,20 @@
 package main
 
-// Values implementation
+/*
+import (
+		"fmt"
+)
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
--- cmd/03-Variables/main.go
+++ cmd/03-Variables/main.go
@@ -1,3 +1,19 @@
 package main
 
-// Variables implementation
+/*
+import (
+		"fmt"
+)
+*/
+
+import "fmt"
+
+func main() {
+	var firstName string = "Srijon"
+	var lastName string = "Paul"
+	initials := "S.P"
+	fmt.Println("His first name is ", firstName, ", last name is ", lastName, " and initials are ", initials)
+	b, c := 1, 2
+	d := -1 == (b - c)
+	fmt.Println(d)
+}
